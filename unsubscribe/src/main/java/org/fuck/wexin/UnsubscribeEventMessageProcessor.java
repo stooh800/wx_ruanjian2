bae6d6963c49ae8c6ffc868ba549bfeb424ad399
Administrator <Administrator@work>
2019-04-24 18:32:52 +0800

    gai

--- unsubscribe/src/main/java/org/fuck/wexin/UnsubscribeEventMessageProcessor.java
+++ unsubscribe/src/main/java/org/fuck/wexin/UnsubscribeEventMessageProcessor.java
@@ -23,7 +23,7 @@ public class UnsubscribeEventMessageProcessor implements EventMessageProcessor {
 	// 表示方法要有事务，在事务内调用数据库查询到的对象的set方法，会自动更新到数据库
 	@Transactional
 	public void onMessage(EventInMessage msg) {
-		if (msg.getEvent().equals("subscribe")) {
+		if (msg.getEvent().equals("unsubscribe")) {
 			System.out.println("取消关注消息处理器: " + msg);
 			// 1.解除用户的关注状态
 			User user = this.userRepository.findByOpenId(msg.getFromUserName());
